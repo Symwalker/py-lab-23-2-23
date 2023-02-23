f082da62e53c5e9d79cce8a9e42c7c33ef6c6877
Shayan hanif <99411978+Symwalker@users.noreply.github.com>
2023-02-23 13:29:01 +0500

    Add files via upload

---
+++ moiz.py
@@ -0,0 +1,123 @@
+#i = 0#
+#while(i < 5):
+    #print(i)
+    #i +=1
+
+#i =5
+#while(i>=0):
+    #print(i * '*')
+    #i -= 1
+#sum = 0
+#n = 0
+#avg = 0
+#amt = 0
+#while(amt >= 0):
+    #amt = int(input("enter your amt:"))
+    #if(amt > 0):
+        #sum += amt
+        #n = n +1
+    #avg = sum/n
+    #print(avg , sum,n)
+
+#list = ["karachi" , "islamabad","lahore","peshawar"]
+#list[2] = "quetta"
+#for items in list:
+    #print(items)
+
+
+#first question 
+#obmarks = int(input("enter obtained marks"))
+#maxmarks = int(input("enter max marks"))
+#perc = (obmarks / maxmarks) * 100
+#print(perc)
+
+#second question
+#radOfCir = int(input("enter radius of circle"))
+#area = (3.14 * radOfCir**2)
+#circum = ((2 * 3.14) * radOfCir)
+#print(area)
+#print(circum)
+
+#third question
+#sellingP = int(input("enter original price of item"))
+#discountedP = int(input("enter discount val"))
+#discount = sellingP - ((sellingP / 100) *discountedP)
+#print(sellingP)
+#print(discount)
+
+#fourth question
+#discount = int(input("enter discount % "))
+#selingp = int(input("enter selingp price "))
+#orignalP = (selingp * 100) / (100-discount)
+#print(orignalP)
+
+#fifth Question
+#voltage = int(input("enter voltage % "))
+#curr = int(input("enter curr % "))
+#power = curr * voltage
+#print(power)
+
+#sixth question
+#h = int(input("enter height "))
+#a = int(input("enter side 1 "))
+#b = int(input("enter side 2 "))
+#k = h * (a +b) / 2
+#print(k)
+
+#seven question
+#num = int(input("enter your num"))
+#if(num % 2==0):
+    #print("even")
+#else:
+    #print("odd")
+#import math 
+#eight question
+#a = int(input("enter s1 "))
+#b = int(input("enter s2 "))
+#c = int(input("enter s3 "))
+#s = (a + b + c) / 2
+#area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+#print(area)
+
+# ninth 1
+#for a in range(1500 , 2701):
+    #if(a %7==0 and a %5==0):
+        #print("multiple by 5 and divisible by 7",a)
+
+#QUESTION 2
+
+
+#num =int(input("enter your num"))
+#fac = 1
+#if(num == 0):
+    #print("1")
+#else:
+    #for i in range(1,num +1):
+       # fac = fac*i
+    #print(fac)
+
+#question 3
+
+
+#list = [22,"sal",True]
+#for i in list:
+   # print(type(i) ,i,end=" ")
+#QUESTION 4
+
+#for a in range(100 , 401):
+    #if(a %2==0 ):
+        #print(a,end=",")
+
+#question 6
+x = int(input("enter your first num"))
+y = int(input("enter your sec num"))
+#if(x%==0 and y%==0):
+        #print(a,end=",")
+
+
+    
+    
+
+
+    
+    
